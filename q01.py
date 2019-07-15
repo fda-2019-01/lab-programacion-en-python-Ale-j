ae3fa354fea1242d49d76754dc538277ce159643
Camarada Perro <Aljaramilloar@unal.edu.co>
2019-07-15 17:02:35 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,13 @@
 ##
 ## Imprima la suma de la segunda columna.
-##
+## 
+archivo = gl.glob("data.csv")
+data = open("data.csv", 'rt').readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t", ",") for line in data]
+data = [line.split(',') for line in data]
+data1 = [i[1] for i in data]
+b = 0
+for i in data1:
+  b = b + int(i)
+print(b)
