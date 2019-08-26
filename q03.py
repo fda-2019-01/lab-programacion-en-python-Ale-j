7e7fd04cc32e7c56876aabcb5a144b8f6bec818e
Al paca <Aljaramilloar@unal.edu.co>
2019-08-26 10:12:20 -0500

    respuesta 3

--- q03.py
+++ q03.py
@@ -7,4 +7,23 @@
 ## C,27
 ## D,23
 ## E,67
-##
+## Soluciión al punto
+# resumen para el resto de puntos
+import glob as gl
+archivo = gl.glob("data.csv")
+datos = open("data.csv", 'rt').readlines()
+datos = [line[:-1] for line in datos]
+datos = [line.replace("\t", ",") for line in datos]
+datos = [line.split(',') for line in datos]
+datos1 = [i[1] for i in datos]
+datos3 = [i[0] for i in datos]
+datos4 = set(datos3)
+d = [i[1] for i in datos]
+d = [int(i) for i in d]
+res = dict.fromkeys(datos3, 0)
+for a, b in zip(datos3, d):
+   res[a] += b
+
+print(*(f"{key}, {value}" for key, value in res.items()), sep="\n")
+
+
