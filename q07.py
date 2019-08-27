89c41dbfe786592015a3f149869b1f495d5ad23e
Al paca <Aljaramilloar@unal.edu.co>
2019-08-27 13:03:01 -0500

    respuesta 7

--- q07.py
+++ q07.py
@@ -16,3 +16,29 @@
 ##    ('9', ['A', 'B', 'E', 'C'])
 ##
 ##
+# resumen para el resto de puntos
+import glob as gl
+archivo = gl.glob("data.csv")
+datos = open("data.csv", 'rt').readlines()
+datos = [line[:-1] for line in datos]
+datos = [line.replace("\t", ",") for line in datos]
+datos = [line.split(',') for line in datos]
+datos = [[line[1], line[0]] for line in datos]
+datos2 = set([line[0] for line in datos])
+list1 = []
+for num in datos2:
+  var = [num]
+  for val in datos:
+    if num == val[0]:
+      var.append(val[1])
+  var = [y for y in var]
+  list1 = list1+[var]
+list2 = [line[0] for line in list1]
+list3 = [line[1:] for line in list1]
+var2=[]
+for i in range(0, len(list1)):
+  var3= (list2[i], list3[i])
+  var2 = var2+[var3]
+var2 = sorted(var2)
+for line in var2:
+  print(line) 
