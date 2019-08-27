7de3f050c00550d47d51afe1b26d3d881243f809
Al paca <Aljaramilloar@unal.edu.co>
2019-08-27 18:12:13 -0500

    respuesta 11

--- q11.py
+++ q11.py
@@ -11,3 +11,23 @@
 ## E,2,3
 ## E,3,3
 ##
+# resumen para el resto de puntos
+import glob as gl
+import csv
+archivo = gl.glob("data.csv")
+datos = open("data.csv", 'r').readlines()
+datos = [line[:-1] for line in datos]
+datos = [line.replace("\t", ",") for line in datos]
+datos = [line.split(',') for line in datos]
+datos2 = [line[0] for line in datos]
+datos = [line[3:] for line in datos]
+datos2_1 = [[fila for fila in line if len(fila) == 1] for line in datos]
+datos2_2 = [[fila for fila in line if len(fila) == 5] for line in datos]
+datos2_3 = [[datos2[i], len(datos2_1[i]), len(datos2_2[i])] for i in range(len(datos))]
+
+doc = gl.glob("respuesta.csv")
+docs = open("respuesta.csv", "w")
+writer = csv.writer(docs, delimiter = ",", quoting = csv.QUOTE_NONNUMERIC) 
+for i in range(len(datos2_3)):
+  writer.writerow(datos2_3[i])
+print(open("respuesta.csv", "r").read())
