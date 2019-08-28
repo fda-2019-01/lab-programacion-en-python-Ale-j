cfc5969b4d622f0415ba2b9c3a23e151950c6cf8
Al paca <Aljaramilloar@unal.edu.co>
2019-08-27 19:05:04 -0500

    respuesta 13

--- q13.py
+++ q13.py
@@ -10,3 +10,24 @@
 ## E,11
 ## E,16
 ##
+# resumen para el resto de puntos
+import glob as gl
+archivo = gl.glob("data.csv")
+datos = open("data.csv", 'r').readlines()
+datos = [line[:-1] for line in datos]
+datos = [line.replace("\t", ",") for line in datos]
+datos = [line.split(',') for line in datos]
+alpha = [line[0] for line in datos]
+variable = [line[5:] for line in datos]
+variable = [[fila for fila in line if len(fila)==5] for line in variable]
+varibale = [[int(fila[4]) for fila in line] for line in variable]
+arab = []
+for fila in variable:
+  a=0
+  for num in row:
+    a = a+num
+  arab.append(a)
+arab
+for i in range(len(arab)):
+  solucion = str(alpha[i])+","+str(arab[i])
+  print(solucion)
